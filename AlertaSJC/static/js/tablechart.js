62028beda031ecbe8924e1ed2adc77fff91901fb
Monica Mota Ocariz <mocariz@localhost.localdomain>
2016-11-21 11:14:41 -0200

    removido excesso de codigo

--- AlertaSJC/static/js/tablechart.js
+++ AlertaSJC/static/js/tablechart.js
@@ -1,40 +1,10 @@
 $(document).ready(function () {
-
-    function createCookie(name, value) {
-        document.cookie = name + "=" + value + "; path=/";
-    }
-
-    function getCookie(cname) {
-        var name = cname + "=";
-        var ca = document.cookie.split(';');
-        for(var i=0; i<ca.length; i++) {
-            var c = ca[i];
-            while (c.charAt(0)==' ') c = c.substring(1);
-            if (c.indexOf(name) != -1) return c.substring(name.length, c.length);
-        }
-        return "";
-    }
-
-    function mostrar(chart, id){
-        if (getCookie(id) == ''){
-            createCookie(id, 'true');
-        }
-        else if (getCookie(id) == 'false'){
-            chart.hide(id);
-        } else {
-            chart.show(id);
-        }
-    }
-
     $('[chart-table]').each(function (i, table) {
         var values = [],
             $table = $(table),
-            chartType = $table.attr('chart-type'),
-            xAxis = parseInt($table.attr('chart-x-axis')),
+            xAxis = 0,
             hideColumns = [],
             campos = [];
-
-        if (!xAxis) xAxis = 0;
 
         $table.find('thead th').each(function (i, cell) {
             var value = $(cell).text().trim();
@@ -75,45 +45,16 @@ $(document).ready(function () {
             });
         });
 
-        var $chart = $($table.attr('chart-table')),
-            xAxisType = $table.attr('chart-x-axis-type'),
-            timeFormat = $table.attr('chart-timeseries-format');
-
+        var $chart = $($table.attr('chart-table'));
         $chart.show();
 
-        if (!xAxisType) xAxisType = 'timeseries';
-        if (!timeFormat) timeFormat = '%d/%m/%Y %H:%M';
-
-        var data = {
-            x: values[xAxis][0],
-            columns: values,
-            type: chartType
-        };
-
-        var xAxisConfig = {};
-
-        xAxisConfig.type = xAxisType;
-
-        if (xAxisType == 'timeseries') {
-            data.xFormat = timeFormat;
-            xAxisConfig.tick = {
-                count: 32,
-                format: timeFormat
-            };
-        }
-
-        if (xAxisType == 'category') {
-            xAxisConfig.height = 90;
-            xAxisConfig.tick = {
-                rotate: -35,
-                fit: true,
-                multiline: false
-            }
-        }
-
-        var chart = c3.generate({
+        c3.generate({
             bindto: $chart.get(0),
-            data: data,
+            data: {
+                x: values[xAxis][0],
+                columns: values,
+                xFormat: '%d/%m/%Y %H:%M'
+            },
             point: {
                 show: false
             },
@@ -126,30 +67,19 @@ $(document).ready(function () {
                 }
             },
             axis: {
-                x: xAxisConfig,
+                x: {
+                  type: 'timeseries',
+                  tick: {
+                      count: 32,
+                      format: '%d/%m/%Y %H:%M'
+                  }
+                },
                 y: {
                     tick: {
                         format: d3.format('.2f')
                     }
                 }
-            },
-            legend: {
-                item: {
-                    onclick: function (id) {
-                        if (getCookie(id) == 'true'){
-                            createCookie(id, 'false');
-                            chart.hide(id);
-                        } else {
-                            createCookie(id, 'true');
-                            chart.show(id);
-                        }
-                    }
-                }
             }
         });
-
-        for (i in campos) {
-            mostrar(chart, campos[i]);
-        }
     });
 });
